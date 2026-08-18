73456fe0b4121e21015bf3cf85f3b7fa88433de3
Christian <christianx111.ls@gmail.com>
2026-08-18 08:00:56 -0300

    feat: created repository

--- user/src/main/java/com/api/techchristian/ms/user/database/repository/UserRepository.java
+++ user/src/main/java/com/api/techchristian/ms/user/database/repository/UserRepository.java
@@ -1,4 +1,9 @@
 package com.api.techchristian.ms.user.database.repository;
 
-public interface UserRepository {
+import com.api.techchristian.ms.user.database.model.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface UserRepository extends JpaRepository<UserEntity, UUID> {
 }
