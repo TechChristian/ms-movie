8fc002f0a2dd98f5ed1d5805019149a094558e3a
Christian <christianx111.ls@gmail.com>
2026-08-18 08:01:17 -0300

    feat: created record dto

--- user/src/main/java/com/api/techchristian/ms/user/dto/UserDto.java
+++ user/src/main/java/com/api/techchristian/ms/user/dto/UserDto.java
@@ -1,4 +1,18 @@
 package com.api.techchristian.ms.user.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
 public class UserDto {
+    public record create(
+            @NotBlank(message = "name is required.")
+            String name,
+            @Email(message = "invalid email format.")
+            @NotBlank(message = "email is required.")
+            String email
+    ){}
+    public record response(
+            String name,
+            String email
+    ){}
 }
