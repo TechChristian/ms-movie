04bdd6f8ff692a2f4cc77dd60a5bbe38b7a170ee
Christian <christianx111.ls@gmail.com>
2026-08-18 08:01:57 -0300

    feat: created mapper for conversions

--- user/src/main/java/com/api/techchristian/ms/user/mapper/UserMapper.java
+++ user/src/main/java/com/api/techchristian/ms/user/mapper/UserMapper.java
@@ -1,4 +1,20 @@
 package com.api.techchristian.ms.user.mapper;
 
+import com.api.techchristian.ms.user.database.model.UserEntity;
+import com.api.techchristian.ms.user.dto.UserDto;
+
 public class UserMapper {
+    public UserEntity toEntity(UserDto.create userCreateDto) {
+        return UserEntity.builder()
+                .name(userCreateDto.name())
+                .email(userCreateDto.email())
+                .build();
+    }
+
+    public UserDto.response toResponse(UserEntity userEntity) {
+        return new UserDto.response(
+                userEntity.getName(),
+                userEntity.getEmail()
+        );
+    }
 }
