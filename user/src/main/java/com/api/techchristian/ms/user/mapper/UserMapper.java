5c84423157a2cb5fb339930a28684a27028f1c02
Christian <christianx111.ls@gmail.com>
2026-08-19 07:09:57 -0300

    refactor: add userId in response method

--- user/src/main/java/com/api/techchristian/ms/user/mapper/UserMapper.java
+++ user/src/main/java/com/api/techchristian/ms/user/mapper/UserMapper.java
@@ -4,15 +4,16 @@ import com.api.techchristian.ms.user.database.model.UserEntity;
 import com.api.techchristian.ms.user.dto.UserDto;
 
 public class UserMapper {
-    public UserEntity toEntity(UserDto.create userCreateDto) {
+    public static UserEntity toEntity(UserDto.create userCreateDto) {
         return UserEntity.builder()
                 .name(userCreateDto.name())
                 .email(userCreateDto.email())
                 .build();
     }
 
-    public UserDto.response toResponse(UserEntity userEntity) {
+    public static UserDto.response toResponse(UserEntity userEntity) {
         return new UserDto.response(
+                userEntity.getUserId(),
                 userEntity.getName(),
                 userEntity.getEmail()
         );
