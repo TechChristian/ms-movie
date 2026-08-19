d2e76eab199cabb82b8613fc453e37263f15db52
Christian <christianx111.ls@gmail.com>
2026-08-19 07:11:13 -0300

    feat: created controller and method post for add new user in application

--- user/src/main/java/com/api/techchristian/ms/user/controller/UserController.java
+++ user/src/main/java/com/api/techchristian/ms/user/controller/UserController.java
@@ -1,4 +1,32 @@
 package com.api.techchristian.ms.user.controller;
 
+import com.api.techchristian.ms.user.dto.UserDto;
+import com.api.techchristian.ms.user.service.UserService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/v1/api/users")
+@RequiredArgsConstructor
+@Slf4j
 public class UserController {
+    private final UserService userService;
+
+    @PostMapping
+    public ResponseEntity<UserDto.response> createUser(@Valid @RequestBody UserDto.create create) {
+        UserDto.response savedUser = userService.addUser(create);
+
+        log.info("User created: {}", savedUser);
+
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(savedUser);
+    }
 }
