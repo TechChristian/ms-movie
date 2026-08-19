ee460b881e7808529433de92db4c51f44a73745a
Christian <christianx111.ls@gmail.com>
2026-08-19 07:08:47 -0300

    feat: created email error response

--- user/src/main/java/com/api/techchristian/ms/user/exception/EmailAlreadyExistsException.java
+++ user/src/main/java/com/api/techchristian/ms/user/exception/EmailAlreadyExistsException.java
@@ -1,7 +1,7 @@
 package com.api.techchristian.ms.user.exception;
 
 public class EmailAlreadyExistsException extends RuntimeException {
-  public EmailAlreadyExistsException(String message) {
-    super(message);
-  }
+    public EmailAlreadyExistsException(String message) {
+        super(message);
+    }
 }
--- user/src/main/java/com/api/techchristian/ms/user/handler/GlobalExceptionHandler.java
+++ user/src/main/java/com/api/techchristian/ms/user/handler/GlobalExceptionHandler.java
@@ -1,4 +1,23 @@
 package com.api.techchristian.ms.user.handler;
 
+import com.api.techchristian.ms.user.exception.EmailAlreadyExistsException;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+@Slf4j
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(EmailAlreadyExistsException.class)
+    public ResponseEntity<ErrorMessage> emailAlreadyExistsException(EmailAlreadyExistsException ex, HttpServletRequest request) {
+        log.error(ex.getMessage());
+        return ResponseEntity.status(HttpStatus.CONFLICT)
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(new ErrorMessage(request, HttpStatus.CONFLICT, ex.getMessage()));
+    }
 }
