9af79cd810c95e586b03970fb0325cf7f6032a17
Christian <christianx111.ls@gmail.com>
2026-08-20 16:20:04 -0300

    feat: created mappers

--- movie/src/main/java/com/api/techchristian/ms/mapper/MovieMapper.java
+++ movie/src/main/java/com/api/techchristian/ms/mapper/MovieMapper.java
@@ -1,4 +1,25 @@
 package com.api.techchristian.ms.mapper;
 
+import com.api.techchristian.ms.database.model.MovieEntity;
+import com.api.techchristian.ms.dto.MovieDto;
+
 public class MovieMapper {
+    public static MovieEntity toEntity (MovieDto.create movieCreateDto){
+        return MovieEntity.builder()
+                .title(movieCreateDto.title())
+                .description(movieCreateDto.description())
+                .genre(movieCreateDto.genre())
+                .releaseYear(movieCreateDto.releaseYear())
+                .build();
+    }
+    public static MovieDto.response toResponse(MovieEntity movieEntity){
+        return new MovieDto.response(
+                movieEntity.getMovieId(),
+                movieEntity.getTitle(),
+                movieEntity.getDescription(),
+                movieEntity.getGenre(),
+                movieEntity.getReleaseYear()
+        );
+    }
+
 }
