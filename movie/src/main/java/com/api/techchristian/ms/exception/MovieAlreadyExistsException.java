faff162e2ded3e190fd735bca974b673aaa456ef
Christian <christianx111.ls@gmail.com>
2026-08-20 16:58:34 -0300

    feat :add error customized

--- movie/src/main/java/com/api/techchristian/ms/exception/MovieAlreadyExistsException.java
+++ movie/src/main/java/com/api/techchristian/ms/exception/MovieAlreadyExistsException.java
@@ -1,7 +1,7 @@
 package com.api.techchristian.ms.exception;
 
 public class MovieAlreadyExistsException extends RuntimeException {
-  public MovieAlreadyExistsException(String message) {
-    super(message);
-  }
+    public MovieAlreadyExistsException(String message) {
+        super(message);
+    }
 }
