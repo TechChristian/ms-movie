19ca1ea5d357ca0ab9fad1004021fcc49d1474d4
Christian <christianx111.ls@gmail.com>
2026-08-20 16:57:36 -0300

    feat: created method for search title in database

--- movie/src/main/java/com/api/techchristian/ms/database/repository/MovieRepository.java
+++ movie/src/main/java/com/api/techchristian/ms/database/repository/MovieRepository.java
@@ -6,4 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.UUID;
 
 public interface MovieRepository extends JpaRepository<MovieEntity, UUID> {
+    boolean existsByTitle (String title);
 }
