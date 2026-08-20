20eefe55c476a4ce20022708d2eaef01ce3e29dc
Christian <christianx111.ls@gmail.com>
2026-08-20 16:21:06 -0300

    docs: organization of docs

--- movie/src/main/java/com/api/techchristian/ms/MovieApplication.java
+++ movie/src/main/java/com/api/techchristian/ms/MovieApplication.java
@@ -1,4 +1,4 @@
-package com.api.techchristian.ms.movie;
+package com.api.techchristian.ms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
