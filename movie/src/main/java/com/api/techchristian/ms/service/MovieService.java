f9da7125baee9975235efe2853506c9313f027c9
Christian <christianx111.ls@gmail.com>
2026-08-20 16:59:19 -0300

    feat: Service implemented in application and method for add new movie

--- movie/src/main/java/com/api/techchristian/ms/service/MovieService.java
+++ movie/src/main/java/com/api/techchristian/ms/service/MovieService.java
@@ -1,4 +1,32 @@
 package com.api.techchristian.ms.service;
 
+import com.api.techchristian.ms.database.model.MovieEntity;
+import com.api.techchristian.ms.database.repository.MovieRepository;
+import com.api.techchristian.ms.dto.MovieDto;
+import com.api.techchristian.ms.exception.MovieAlreadyExistsException;
+import com.api.techchristian.ms.mapper.MovieMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
 public class MovieService {
+    private final MovieRepository movieRepository;
+
+    @Transactional
+    public MovieDto.response addMovie (MovieDto.create createMovie){
+
+        MovieEntity movieRequestToEntity = MovieMapper.toEntity(createMovie);
+
+        boolean existsMovie = movieRepository.existsByTitle(movieRequestToEntity.getTitle());
+
+        if(existsMovie){
+            throw new MovieAlreadyExistsException("The Movie already exists " + createMovie.title());
+        }
+
+        var savedMovie = movieRepository.save(movieRequestToEntity);
+
+        return MovieMapper.toResponse(savedMovie);
+    }
 }
