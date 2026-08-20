dd84b5765fe60e6cf13c56268c04523b1c98dec9
Christian <christianx111.ls@gmail.com>
2026-08-20 16:19:31 -0300

    feat: created movie dto

--- movie/src/main/java/com/api/techchristian/ms/dto/MovieDto.java
+++ movie/src/main/java/com/api/techchristian/ms/dto/MovieDto.java
@@ -1,4 +1,30 @@
 package com.api.techchristian.ms.dto;
 
+import com.api.techchristian.ms.database.enums.GenreEnum;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+import java.util.UUID;
+
 public class MovieDto {
+    public record create(
+            @NotBlank(message = "the field title is required.")
+            String title,
+
+            @NotBlank(message = "the field description is required.")
+            String description,
+
+            @NotNull(message = "the field genre is required.")
+            GenreEnum genre,
+
+            @NotNull(message = "the field release year is required.")
+            int releaseYear
+    ){}
+    public record response(
+            UUID movieId,
+            String title,
+            String description,
+            GenreEnum genre,
+            int releaseYear
+    ){}
 }
