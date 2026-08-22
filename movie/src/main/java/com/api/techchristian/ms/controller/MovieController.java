0ab47b1b800fcd2ad1e1c7d618b96ad0bf14ef9b
Christian <christianx111.ls@gmail.com>
2026-08-21 22:21:02 -0300

    feat: creation http get for response all movies in application

--- movie/src/main/java/com/api/techchristian/ms/controller/MovieController.java
+++ movie/src/main/java/com/api/techchristian/ms/controller/MovieController.java
@@ -7,10 +7,9 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("v1/api/movies")
@@ -25,5 +24,13 @@ public class MovieController {
         log.info("Movie added : {}", responseMovie);
 
         return ResponseEntity.status(HttpStatus.CREATED).body(responseMovie);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<MovieDto.response>> getAllMovies() {
+        List<MovieDto.response> responseMovies = movieService.getAllMovies();
+        log.info("All movies found : {}", responseMovies);
+
+        return ResponseEntity.status(HttpStatus.OK).body(responseMovies);
     }
 }
