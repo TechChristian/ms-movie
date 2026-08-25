114f3934a7c68c11434f6ffa477e82437be1dcc9
Christian <christianx111.ls@gmail.com>
2026-08-25 08:33:36 -0300

    feat: created ticket dto, record create and response

--- ticket/src/main/java/com/api/techchristian/ms/ticket/dto/TicketDto.java
+++ ticket/src/main/java/com/api/techchristian/ms/ticket/dto/TicketDto.java
@@ -1,4 +1,30 @@
 package com.api.techchristian.ms.ticket.dto;
 
+import com.api.techchristian.ms.ticket.database.enums.TicketStatus;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+
+import java.util.UUID;
+
 public class TicketDto {
+    public record Create(
+            @NotNull(message = "user id is required.")
+            UUID userId,
+            @NotNull(message = "movie id is required.")
+            UUID movieId,
+            @Min(value = 1, message = "A seat between 1 and 30 is required.")
+            @Max(value = 30, message ="A seat between 1 and 30 is required.")
+            @NotNull(message = "seat is required.")
+            int seat
+    ) {
+    }
+
+    public record Response(
+            UUID userId,
+            UUID movieId,
+            int seat,
+            TicketStatus status
+    ) {
+    }
 }
