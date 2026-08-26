86f27500f70ed35ea7c60d012bb4aa803c8a79c3
Christian <christianx111.ls@gmail.com>
2026-08-26 08:23:01 -0300

    feat: created endpoint for get user

--- user/src/main/java/com/api/techchristian/ms/user/controller/UserController.java
+++ user/src/main/java/com/api/techchristian/ms/user/controller/UserController.java
@@ -7,10 +7,9 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/v1/api/users")
@@ -28,5 +27,15 @@ public class UserController {
         return ResponseEntity
                 .status(HttpStatus.CREATED)
                 .body(savedUser);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<UserDto.response> getUser(@PathVariable UUID id) {
+        UserDto.response user = userService.getUser(id);
+        log.info("User get: {}", user);
+
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(user);
     }
 }
