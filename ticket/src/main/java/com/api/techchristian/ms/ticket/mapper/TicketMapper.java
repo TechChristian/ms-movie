8856820b22c3e4d967c17b474be8c370101d1a21
Christian <christianx111.ls@gmail.com>
2026-08-26 08:10:46 -0300

    feat: created ticket mapper

--- ticket/src/main/java/com/api/techchristian/ms/ticket/mapper/TicketMapper.java
+++ ticket/src/main/java/com/api/techchristian/ms/ticket/mapper/TicketMapper.java
@@ -1,4 +1,24 @@
 package com.api.techchristian.ms.ticket.mapper;
 
+import com.api.techchristian.ms.ticket.database.model.TicketEntity;
+import com.api.techchristian.ms.ticket.dto.TicketDto;
+
 public class TicketMapper {
+    public static TicketEntity toEntity(TicketDto.Create createTicketDto) {
+        return TicketEntity.builder()
+                .userId(createTicketDto.userId())
+                .movieId(createTicketDto.movieId())
+                .seat(createTicketDto.seat())
+                .build();
+
+    }
+
+    public static TicketDto.Response toResponse(TicketEntity ticketEntity){
+        return new TicketDto.Response(
+                ticketEntity.getUserId(),
+                ticketEntity.getMovieId(),
+                ticketEntity.getSeat(),
+                ticketEntity.getStatus()
+        );
+    }
 }
