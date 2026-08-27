feb919815b84aa4c452b82b3b0752b8cf84f47eb
Christian <christianx111.ls@gmail.com>
2026-08-27 12:10:28 -0300

    feat: creation ticket producer for submit put one queue to message

--- ticket/src/main/java/com/api/techchristian/ms/ticket/producer/TicketProducer.java
+++ ticket/src/main/java/com/api/techchristian/ms/ticket/producer/TicketProducer.java
@@ -1,4 +1,29 @@
 package com.api.techchristian.ms.ticket.producer;
 
-public class PaymentRequest {
+import com.api.techchristian.ms.ticket.database.model.TicketEntity;
+import com.api.techchristian.ms.ticket.dto.PaymentDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class TicketProducer {
+    private final RabbitTemplate rabbitTemplate;
+
+    @Value(value = "${broker.queue.ticket.name}")
+    private String ticketCreated;
+
+    public void createPayment(TicketEntity ticketEntity) {
+        PaymentDto paymentCreateDto = new PaymentDto();
+
+        paymentCreateDto.setTicketId(ticketEntity.getTicketId());
+        paymentCreateDto.setUserId(ticketEntity.getUserId());
+        paymentCreateDto.setMovieId(ticketEntity.getMovieId());
+        paymentCreateDto.setPrice(ticketEntity.getPrice());
+
+        rabbitTemplate.convertAndSend("", ticketCreated, paymentCreateDto);
+    }
+
 }
