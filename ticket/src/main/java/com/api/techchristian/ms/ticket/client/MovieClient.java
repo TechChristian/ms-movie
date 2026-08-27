c261557bdaec825c7b879cfdf8e72461a1dbd212
Christian <christianx111.ls@gmail.com>
2026-08-27 11:50:57 -0300

    feat: Creation of REST client methods for the ticket service to communicate with ms-movie and ms-user.

--- ticket/src/main/java/com/api/techchristian/ms/ticket/client/MovieClient.java
+++ ticket/src/main/java/com/api/techchristian/ms/ticket/client/MovieClient.java
@@ -1,4 +1,23 @@
 package com.api.techchristian.ms.ticket.client;
 
+import com.api.techchristian.ms.ticket.dto.MovieResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestClient;
+
+import java.util.UUID;
+
+@Component
 public class MovieClient {
+    private final RestClient restClient;
+
+    public MovieClient(RestClient restClient) {
+        this.restClient = restClient;
+    }
+
+    public MovieResponse getMovie(UUID movieId){
+        return restClient.get()
+                .uri("http://localhost:8081/v1/api/movies/{id}", movieId)
+                .retrieve()
+                .body(MovieResponse.class);
+    }
 }
--- ticket/src/main/java/com/api/techchristian/ms/ticket/client/UserClient.java
+++ ticket/src/main/java/com/api/techchristian/ms/ticket/client/UserClient.java
@@ -1,4 +1,24 @@
 package com.api.techchristian.ms.ticket.client;
 
+import com.api.techchristian.ms.ticket.dto.MovieResponse;
+import com.api.techchristian.ms.ticket.dto.UserResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestClient;
+
+import java.util.UUID;
+
+@Component
 public class UserClient {
+    private final RestClient restClient;
+
+    public UserClient(RestClient restClient) {
+        this.restClient = restClient;
+    }
+
+    public UserResponse getUser(UUID userId){
+        return restClient.get()
+                .uri("http://localhost:8080/v1/api/users/{id}", userId)
+                .retrieve()
+                .body(UserResponse.class);
+    }
 }
