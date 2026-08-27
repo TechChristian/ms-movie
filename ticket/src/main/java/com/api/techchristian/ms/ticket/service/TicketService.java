5a0998777df90c9b55d3e29a89c5459c3a597a0f
Christian <christianx111.ls@gmail.com>
2026-08-27 12:12:13 -0300

    feat: creation ticket service and association informations to ticket entity

--- ticket/src/main/java/com/api/techchristian/ms/ticket/service/TicketService.java
+++ ticket/src/main/java/com/api/techchristian/ms/ticket/service/TicketService.java
@@ -1,4 +1,52 @@
 package com.api.techchristian.ms.ticket.service;
 
+import com.api.techchristian.ms.ticket.client.MovieClient;
+import com.api.techchristian.ms.ticket.client.UserClient;
+import com.api.techchristian.ms.ticket.database.enums.TicketStatus;
+import com.api.techchristian.ms.ticket.database.model.TicketEntity;
+import com.api.techchristian.ms.ticket.database.repository.TicketRepository;
+import com.api.techchristian.ms.ticket.dto.TicketDto;
+import com.api.techchristian.ms.ticket.mapper.TicketMapper;
+import com.api.techchristian.ms.ticket.producer.TicketProducer;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
 public class TicketService {
+    private final TicketRepository ticketRepository;
+    private final MovieClient movieClient;
+    private final UserClient userClient;
+    private final TicketProducer ticketProducer;
+
+    @Transactional
+    public TicketDto.Response addTicket(TicketDto.Create createTicket){
+
+        var movie = movieClient.getMovie(createTicket.movieId());
+        log.info("Creating ticket for movie: {}", movie);
+
+        var user = userClient.getUser(createTicket.userId());
+        log.info("Creating ticket for user: {}", user);
+
+        var ticket = TicketMapper.toEntity(createTicket);
+
+        ticket.setMovieId(movie.movieId());
+        ticket.setUserId(user.userId());
+        ticket.setPrice(movie.price());
+        ticket.setSeat(createTicket.seat());
+        ticket.setStatus(TicketStatus.PENDING);
+
+        var savedTicket = ticketRepository.save(ticket);
+
+        log.info("Saved ticket: {}", savedTicket);
+
+        ticketProducer.createPayment(savedTicket);
+
+        return TicketMapper.toResponse(savedTicket);
+
+    }
+
 }
