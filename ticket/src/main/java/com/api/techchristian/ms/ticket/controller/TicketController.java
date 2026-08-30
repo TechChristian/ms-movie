3fe62db8d73a5412902b3ef8d22fd3e1ee4155c1
Christian <christianx111.ls@gmail.com>
2026-08-30 16:08:24 -0300

    feat: creation controller of submit ticket to payment queue

--- ticket/src/main/java/com/api/techchristian/ms/ticket/controller/TicketController.java
+++ ticket/src/main/java/com/api/techchristian/ms/ticket/controller/TicketController.java
@@ -1,4 +1,30 @@
 package com.api.techchristian.ms.ticket.controller;
 
+import com.api.techchristian.ms.ticket.dto.TicketDto;
+import com.api.techchristian.ms.ticket.service.TicketService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("v1/api/ticket")
+@RequiredArgsConstructor
+@Slf4j
 public class TicketController {
+    private final TicketService ticketService;
+
+    @PostMapping
+    public ResponseEntity<TicketDto.Response> addTicket (@RequestBody @Valid TicketDto.Create ticketDto) {
+        TicketDto.Response response = ticketService.addTicket(ticketDto);
+        log.info("Ticket added successfully {}", response);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+
+    }
 }
