f7868c83e7826ab3c12e0096ab77bd32bcac8c68
Christian <christianx111.ls@gmail.com>
2026-09-03 17:50:53 -0300

    feat: producer for submit result of status ticket: PAID or CANCELLED

--- payment/src/main/java/com/api/techchristian/ms/payment/producer/PaymentProducer.java
+++ payment/src/main/java/com/api/techchristian/ms/payment/producer/PaymentProducer.java
@@ -1,4 +1,21 @@
-package com.api.techchristian.ms.producer;
+package com.api.techchristian.ms.payment.producer;
 
+import com.api.techchristian.ms.payment.dto.PaymentResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
 public class PaymentProducer {
+    private final RabbitTemplate rabbitTemplate;
+
+    @Value("${broker.queue.ticket.ticket-payment}")
+    private String paymentQueue;
+
+    public void sendPayment(PaymentResponse paymentResponse) {
+        rabbitTemplate.convertAndSend(paymentQueue, paymentResponse);
+    }
+
 }
